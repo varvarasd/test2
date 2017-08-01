32be64a40e310788f858873d98faed72d5f0e173
Daniel Varvara <varvara_sd@yahoo.co.uk>
2017-08-01 15:09:08 +0300

    import all css in js coresponding files

--- src/component/ButtonPanel.js
+++ src/component/ButtonPanel.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Button from './Button';
 import PropTypes from 'prop-types';
-
+import './ButtonPanel.css';
 
 class ButtonPanel extends React.Component {
     render() {
